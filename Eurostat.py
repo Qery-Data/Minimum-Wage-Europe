967713c70ccd2197758b6842e086f0e4b01a14d8
aloenne-qery <alf.lonne@qery.no>
2025-02-01 14:22:01 +0100

    .

--- Eurostat.py
+++ Eurostat.py
@@ -40,7 +40,7 @@ flag_codes = {
 dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/earn_mw_cur?lang=en&lastTimePeriod=2&currency=EUR&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=US')
 df = dataset.write('dataframe')
 df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Time', values='value')
-df_new = df_new.dropna()
+df_new = df_new.dropna(how='all')
 df_new.index = [f"{flag_codes[country]} {country}" for country in df_new.index]
 df_new.to_csv('data/Eurostat_Minimum_Wage_EU_EUR.csv', index=True)
 
@@ -48,6 +48,6 @@ df_new.to_csv('data/Eurostat_Minimum_Wage_EU_EUR.csv', index=True)
 dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/earn_mw_cur?lang=en&lastTimePeriod=2&currency=PPS&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=US')
 df = dataset.write('dataframe')
 df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Time', values='value')
-df_new = df_new.dropna()
+df_new = df_new.dropna(how='all')
 df_new.index = [f"{flag_codes[country]} {country}" for country in df_new.index]
 df_new.to_csv('data/Eurostat_Minimum_Wage_EU_PPS.csv', index=True)
